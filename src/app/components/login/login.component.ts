637e8ac87df9a47a5ce3d84727064900ad10a8c8
SeniorDev118 <pewallin0527@gmail.com>
2018-10-01 22:49:59 +0300

    add login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onFormSubmit(data) {
+    console.log(data);
+    // this.http.post('http://localhost:8080/api/checkuser', data)
+    // .subscribe(data => {
+    //   if(data["_body"] != 'invalid') {
+    //     this.router.navigateByUrl('/conversation'); 
+    //   } else {
+    //     alert("Invalid User");
+    //   }
+    // });
+    localStorage.setItem('email', data.email);
   }
 
 }
