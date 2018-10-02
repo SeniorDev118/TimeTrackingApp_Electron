304b81a7b2182a83c806fafe1be980a7bff7c409
SeniorDev118 <pewallin0527@gmail.com>
2018-10-02 08:57:33 +0300

    add main timetracking UI

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'electron';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,19 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  items = ["Angular 6", "React", "Underscore"];
+  newItem = "";
+  pushItem = function() {
+    if (this.newItem != "") {
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem = function(index) {
+    this.items.splice(index, 1);
   }
 
 }
